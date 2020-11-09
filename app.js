18aa8d184e8e782009482ed7312c4390ba718eac
leeminewook <63051680+leeminewook@users.noreply.github.com>
2020-11-09 10:16:09 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -8,12 +8,16 @@ const login = require('./router/login');
 const register = require('./router/register');
 const email_check =require('./router/email_check');
 const nickname_check = require('./router/nickname_check');
+const profile = require('./router/profile');
+const creat = require('./router/creat');
+const P_delete = require('./router/P_delete');
+
 
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(login,register,email_check,nickname_check);
+app.use(login,register,email_check,nickname_check,profile,creat,P_delete);
 
 app.listen(8080, () => {
     console.log("server start");
